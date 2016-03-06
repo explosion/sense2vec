60d4102b867fad876a6dc84d9151611fb4940d79
Henning Peters <pete@dexterslab.de>
2016-03-06 15:48:37 +0100

    default name

--- sense2vec/__init__.py
+++ sense2vec/__init__.py
@@ -2,7 +2,7 @@ from . import util
 from .vectors import VectorMap
 
 
-def load(name, via=None):
+def load(name=None, via=None):
     package = util.get_package_by_name(name, via=via)
     vector_map = VectorMap(128)
     vector_map.load(package.path)
