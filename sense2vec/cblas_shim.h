b05fffb264c8b7e32b46452b0ab085b640a1308f
init-random <chewning_keith@yahoo.com>
2016-03-07 22:06:25 -0500

    implement sdot and snrm2 with eigen

--- sense2vec/cblas_shim.h
+++ sense2vec/cblas_shim.h
@@ -14,24 +14,25 @@ int _use_blas()
 }
 #else // USE_BLAS
 
+Eigen::VectorXf arr_to_vecf(const int N, const float *arr) {
+    // need non-const for vector
+    Eigen::Map<Eigen::VectorXf> v((float*)arr, N);
+    return v;
+}
+
+
 float cblas_snrm2(const int N, const float *X, const int incX)
 {
-    float norm = 0;
-    for (int i=0; i<N; i++) {
-        norm += pow(X[i], 2);
-    }
-    return sqrtf(norm);
+    Eigen::VectorXf v = arr_to_vecf(N, X);
+    return v.norm();
 }
 
 float cblas_sdot(const int N, const float *X, const int incX,
                  const float *Y, const int incY)
 {
-    // need non-const for vector
-    float *pX = (float*)X;
-    float *pY = (float*)Y;
-    Eigen::Map<Eigen::VectorXf> a(pX, N);
-    Eigen::Map<Eigen::VectorXf> b(pY, N);
-    return -1.; //a.dot(b);
+    Eigen::VectorXf a = arr_to_vecf(N, X);
+    Eigen::VectorXf b = arr_to_vecf(N, Y);
+    return a.dot(b);
 }
 
 int _use_blas()
