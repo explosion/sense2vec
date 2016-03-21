c2c6794f25b15915109dd9751160e19bf3bffdc7
Henning Peters <pete@dexterslab.de>
2016-03-21 18:21:48 +0100

    vectorize dot/norm

--- include/cblas_shim.h
+++ include/cblas_shim.h
@@ -10,24 +10,55 @@ int _use_blas()
     return 1;
 }
 #else // USE_BLAS
-#include <math.h>
+#if defined(_MSC_VER)
+/* Microsoft C/C++-compatible compiler */
+#include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+/* GCC-compatible compiler, targeting x86/x86-64 */
+#include <x86intrin.h>
+#endif
 
-float cblas_snrm2(const int N, const float *X, const int incX)
+float cblas_snrm2(const int N, const float *m1, const int incX)
 {
-    float norm = 0;
-    for (int i=0; i<N; i++) {
-        norm += pow(X[i], 2);
+    if (N % 4 != 0) {
+        fprintf(stderr, "cblas_snrm2() expects N to be a multiple of 4.\n");
+        exit(EXIT_FAILURE);
     }
+
+    float norm = 0;
+    __m128 X;
+    __m128 Z = _mm_setzero_ps();
+
+    for (int i=0; i<N; i+=4) {
+        X = _mm_load_ps(&m1[i]);
+        X = _mm_mul_ps(X, X);
+        Z = _mm_add_ps(X, Z);
+    }
+
+    norm += Z[0] + Z[1] + Z[2] + Z[3];
     return sqrtf(norm);
 }
 
-float cblas_sdot(const int N, const float *X, const int incX,
-                 const float *Y, const int incY)
+float cblas_sdot(const int N, const float *m1, const int incX,
+                 const float *m2, const int incY)
 {
-    float dot = 0;
-    for (int i=0; i<N; i++) {
-        dot += X[i] * Y[i];
+    if (N % 4 != 0) {
+        fprintf(stderr, "cblas_sdot() expects N to be a multiple of 4.\n");
+        exit(EXIT_FAILURE);
     }
+
+    float dot = 0;
+    __m128 X, Y;
+    __m128 Z = _mm_setzero_ps();
+
+    for (int i=0; i<N; i+=4) {
+        X = _mm_load_ps(&m1[i]);
+        Y = _mm_load_ps(&m2[i]);
+        X = _mm_mul_ps(X, Y);
+        Z = _mm_add_ps(X, Z);
+    }
+
+    dot += Z[0] + Z[1] + Z[2] + Z[3];
     return dot;
 }
 
