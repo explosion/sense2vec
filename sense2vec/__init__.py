d2854af24dcfe951e144236a3efa121ff29b36c4
ines <ines@ines.io>
2018-04-10 15:22:25 +0200

    Raise error early if vectors path doesn't exist (see #44)

--- sense2vec/__init__.py
+++ sense2vec/__init__.py
@@ -1,11 +1,15 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+from os import path
+
 from .vectors import VectorMap
 from .about import __version__
 
 
 def load(vectors_path):
+    if not path.exists(vectors_path):
+        raise IOError("Can't find data directory: {}".format(vectors_path))
     vector_map = VectorMap(128)
     vector_map.load(vectors_path)
     return vector_map
