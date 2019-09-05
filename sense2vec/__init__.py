10e234740e0b87bba30c76a51b93c9ca22d6d479
Ines Montani <ines@ines.io>
2019-09-05 15:00:34 +0200

    Rewrite transform_doc logic

--- sense2vec/__init__.py
+++ sense2vec/__init__.py
@@ -15,19 +15,35 @@ def load(vectors_path):
     return vector_map
 
 
+def filter_spans(spans):
+    # Filter a sequence of spans so they don't contain overlaps
+    get_sort_key = lambda span: (span.end - span.start, span.start)
+    sorted_spans = sorted(spans, key=get_sort_key, reverse=True)
+    result = []
+    seen_tokens = set()
+    for span in sorted_spans:
+        if span.start not in seen_tokens and span.end - 1 not in seen_tokens:
+            result.append(span)
+            seen_tokens.update(range(span.start, span.end))
+    return result
+
+
 def transform_doc(doc):
     """
     Transform a spaCy Doc to match the sense2vec format: merge entities
     into one token and merge noun chunks without determiners.
     """
-    # if not doc.is_tagged:
-    #    raise ValueError("Can't run sense2vec: document not tagged.")
-    for ent in doc.ents:
-        ent.merge(tag=ent.root.tag_, lemma=ent.root.lemma_, ent_type=ent.label_)
+    spans = list(doc.ents)
     for np in doc.noun_chunks:
         while len(np) > 1 and np[0].dep_ not in ("advmod", "amod", "compound"):
             np = np[1:]
-        np.merge(tag=np.root.tag_, lemma=np.root.lemma_, ent_type=np.root.ent_type_)
+        spans.append(np)
+    spans = filter_spans(spans)
+    with doc.retokenize() as retokenizer:
+        for span in spans:
+            root = span.root
+            attrs = {"tag": root.tag_, "lemma": root.lemma_, "ent_type": root.ent_type_}
+            retokenizer.merge(span, attrs=attrs)
     return doc
 
 
