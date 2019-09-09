580f0ca1c852c0af670fd2bb3e61e5af660eec11
Ines Montani <ines@ines.io>
2019-09-09 15:14:53 +0200

    Set version to 1.0.0a1

--- sense2vec/about.py
+++ sense2vec/about.py
@@ -1,5 +1,5 @@
 __title__ = "sense2vec"
-__version__ = "1.1.0a0"
+__version__ = "1.0.0a1"
 __summary__ = "Use NLP to go beyond vanilla word2vec"
 __uri__ = "https://github.com/explosion/sense2vec"
 __author__ = "Explosion AI"
