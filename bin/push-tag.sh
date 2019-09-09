97537442628883597f1b15c3e562b6eb0d74bf55
Matthew Honnibal <honnibal+gh@gmail.com>
2019-09-09 12:39:05 +0200

    Fix name

--- bin/push-tag.sh
+++ bin/push-tag.sh
@@ -9,7 +9,7 @@ git checkout $1
 git pull origin $1
 git push origin $1
 
-version=$(grep "__version__ = " spacy/about.py)
+version=$(grep "__version__ = " sense2vec/about.py)
 version=${version/__version__ = }
 version=${version/\'/}
 version=${version/\'/}
