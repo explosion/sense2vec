e85fd9639f5a50c8f397b9b708c1e7e4ef2cc006
Ines Montani <ines@ines.io>
2019-09-27 15:15:15 +0200

    Add test data and model tests stub

---
+++ tests/test_model.py
@@ -0,0 +1,17 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+import pytest
+from pathlib import Path
+from sense2vec import Sense2Vec
+
+
+@pytest.fixture
+def s2v():
+    data_path = Path(__file__).parent / "data"
+    return Sense2Vec().from_disk(data_path)
+
+
+def test_most_similar(s2v):
+    assert "beekeepers|NOUN" in s2v
+    result = s2v.most_similar(["beekeepers|NOUN"])
