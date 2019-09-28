688d1429ff878704d7e664f803948a724f78699c
Ines Montani <ines@ines.io>
2019-09-28 02:42:09 +0200

    Update train.py

--- bin/train.py
+++ bin/train.py
@@ -4,28 +4,35 @@ from gensim.models.word2vec import PathLineSentences
 from sense2vec import Sense2Vec
 from sense2vec.util import split_key
 import plac
+import logging
 
 
 @plac.annotations(
     in_dir=("Location of input directory", "positional", None, str),
-    out_file=("Location of output file", "positional", None, str),
+    out_dir=("Location of output directory", "positional", None, str),
     n_workers=("Number of workers", "option", "n", int),
     size=("Dimension of the word vectors", "option", "d", int),
     window=("Context window size", "option", "w", int),
     min_count=("Min count", "option", "m", int),
     negative=("Number of negative samples", "option", "g", int),
     nr_iter=("Number of iterations", "option", "i", int),
+    verbose=("Log debugging info", "flag", "V", bool),
 )
 def main(
     in_dir,
-    out_file,
+    out_dir,
     negative=5,
     n_workers=4,
     window=5,
     size=128,
     min_count=10,
     nr_iter=2,
+    verbose=False,
 ):
+    if verbose:
+        logging.basicConfig(
+            format="%(asctime)s - %(message)s", datefmt="%H:%M:%S", level=logging.INFO
+        )
     w2v_model = Word2Vec(
         size=size,
         window=window,
@@ -55,11 +62,11 @@ def main(
         _, sense = split_key(string)
         all_senses.add(sense)
     s2v = Sense2Vec(shape=(len(vectors), size), senses=all_senses)
-    for string, _, vector in vectors:
-        s2v.add(string, vector)
+    for string, freq, vector in vectors:
+        s2v.add(string, vector, freq)
     print("Saving the model...")
-    s2v.to_disk(out_file)
-    print(f"Saved model to file: {out_file}")
+    s2v.to_disk(out_dir)
+    print(f"Saved model to directory: {out_dir}")
 
 
 if __name__ == "__main__":
