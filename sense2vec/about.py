8a4706ec186894cfa9743b57c1fc9449380b9d0d
Ines Montani <ines@ines.io>
2019-10-04 18:01:03 +0200

    Use setup.cfg

--- sense2vec/about.py
+++ sense2vec/about.py
@@ -1,7 +1,1 @@
-__title__ = "sense2vec"
 __version__ = "1.0.0a2"
-__summary__ = "Use NLP to go beyond vanilla word2vec"
-__uri__ = "https://github.com/explosion/sense2vec"
-__author__ = "Explosion"
-__email__ = "contact@explosion.ai"
-__license__ = "MIT"
--- setup.py
+++ setup.py
@@ -9,52 +9,13 @@ from setuptools import setup, find_packages
 def setup_package():
     package_name = "sense2vec"
     root = os.path.abspath(os.path.dirname(__file__))
-
     # Read in package meta from about.py
     about_path = os.path.join(root, package_name, "about.py")
     with io.open(about_path, encoding="utf8") as f:
         about = {}
         exec(f.read(), about)
 
-    # Get readme
-    readme_path = os.path.join(root, "README.md")
-    with io.open(readme_path, encoding="utf8") as f:
-        readme = f.read()
-
-    setup(
-        name="sense2vec",
-        description=about["__summary__"],
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        author=about["__author__"],
-        author_email=about["__email__"],
-        url=about["__uri__"],
-        version=about["__version__"],
-        license=about["__license__"],
-        packages=find_packages(),
-        install_requires=["spacy>=2.1.0", "numpy>=1.15.0", "srsly>=0.1.0"],
-        python_requires=">=3.6",
-        entry_points={
-            "spacy_factories": ["sense2vec = sense2vec:Sense2VecComponent.from_nlp"],
-            "prodigy_recipes": [
-                "sense2vec_teach = sense2vec:prodigy_recipes.teach",
-                "sens2vec_to_patterns = sense2vec:prodigy_recipes.to_patterns",
-            ],
-        },
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "Intended Audience :: Science/Research",
-            "Topic :: Scientific/Engineering :: Artificial Intelligence",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: POSIX :: Linux",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-        ],
-        zip_safe=False,
-    )
+    setup(name="sense2vec", version=about["__version__"], packages=find_packages())
 
 
 if __name__ == "__main__":
