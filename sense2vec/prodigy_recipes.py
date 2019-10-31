8dcc3c33a46d9b9f309e3d87e874a356ab18ad0a
Ines Montani <ines@ines.io>
2019-11-01 00:45:42 +0100

    Update recipe

--- sense2vec/prodigy_recipes.py
+++ sense2vec/prodigy_recipes.py
@@ -4,83 +4,16 @@ from prodigy.util import log, split_string, set_hashes, TASK_HASH_ATTR
 from sense2vec import Sense2Vec
 import srsly
 import spacy
+import random
+from wasabi import Printer
+from collections import defaultdict
+import copy
 
 
 HTML_TEMPLATE = """
 <span style="font-size: {{theme.largeText}}px">{{word}}</span>
 <strong style="opacity: 0.75">{{sense}}</strong>
 """
-
-@prodigy.recipe(
-    "sense2vec.evaluate",
-    dataset=("Dataset to save annotations to", "positional", None, str),
-    model=("Name or path of sense2vec model", "positional", None, str),
-    tasks=("File with similarity triples to ask about. If none, questions will be generated.", "positional", None)
-)
-def evaluate(
-    dataset,
-    model,
-    tasks=None,
-):
-    """Evaluate a word vectors model by asking providing questions triples:
-    is word A more similar to word B, or to word C? If the human mostly agrees
-    with the model, the vectors model is good.
-    """
-    nlp = spacy.load(model)
-    
-    def get_stream(s2v):
-        keys = list(s2v.vectors.keys())
-        while True:
-            a, b, c = random.sample(keys, 3)
-            sim_ab = self.vectors.similarity(a, b)
-            sim_ac = self.vectors.similarity(a, c)
-            sim_bc = self.vectors.similarity(b, c)
-            wordA = s2v.strings[a]
-            wordB = s2v.strings[b]
-            wordC = s2v.strings[c]
-            confidence = 1. - (min(sim_ab, sim_ac) / max(sim_ab, sim_ac))
-
-            if sim_ab > sim_ac:
-                mapping = {"agree": accept, "disagree": reject}
-            else:
-                mapping = {"disagree": accept, "agree": reject}
-
-            task = {
-                "input": { "text": s2v.strings[a] },
-                "accept": { "text": s2v.strings[wordB] },
-                "reject": { "text": s2v.strings[wordC] },
-                "mapping": mapping,
-                "similarities": [
-                    {"pair": [wordA, wordB], "score": sim_ab},
-                    {"pair": [wordA, wordC], "score": sim_ac},
-                    {"pair": [wordB, wordC], "score": sim_bc}
-                ],
-                "confidence": confidence
-            }
-            task = set_hashes(task)
-            yield task
-
-    def update(answers):
-        """Updates accept_keys so that the stream can find new phrases."""
-        log(f"RECIPE: Updating with {len(answers)} answers")
-        loss = 0.
-        for eg in answers:
-            human = eg["answer"]
-            if eg["answer"] in ("accept", "reject"):
-                if eg["mapping"][eg["answer"]] == "agree":
-                    right += 1
-                else:
-                    wrong += 1
-                    loss += eg["confidence"]
-    return {
-        "view_id": "compare",
-        "dataset": dataset,
-        "stream": stream,
-        "update": update,
-        "config": {"batch_size": batch_size, "html_template": HTML_TEMPLATE},
-    }
-
-   
 
 
 @prodigy.recipe(
@@ -221,3 +154,104 @@ def to_patterns(
     if not dry:
         srsly.write_jsonl(output_file, patterns)
     return patterns
+
+
+@prodigy.recipe(
+    "sense2vec.evaluate",
+    dataset=("Dataset to save annotations to", "positional", None, str),
+    vectors_path=("Path to pretrained sense2vec vectors", "positional", None, str),
+    senses=("The senses to use (all if not set)", "option", "s", split_string),
+    n_freq=("Number of most frequent entries to limit to", "option", "f", int),
+    threshold=("Similarity threshold to consider examples", "option", "t", float),
+    eval_whole=("Evaluate whole dataset instead of session", "flag", "E", bool),
+)
+def evaluate(
+    dataset, vectors_path, senses=None, n_freq=100_000, threshold=0.7, eval_whole=False
+):
+    """Evaluate a word vectors model by asking providing questions triples:
+    is word A more similar to word B, or to word C? If the human mostly agrees
+    with the model, the vectors model is good.
+    """
+    random.seed(0)
+    log("RECIPE: Starting recipe sense2vec.evaluate", locals())
+    s2v = Sense2Vec().from_disk(vectors_path)
+    log("RECIPE: Loaded sense2vec vectors", vectors_path)
+
+    def get_stream():
+        html = "{} <strong style='opacity: 0.75; font-size: 14px; padding-left: 10px'>{}</strong>"
+        # Limit to most frequent entries
+        keys = [key for key, _ in s2v.frequencies[:n_freq]]
+        keys_by_sense = defaultdict(set)
+        for key in keys:
+            sense = s2v.split_key(key)[1]
+            if senses is None or sense in senses:
+                keys_by_sense[sense].add(key)
+        keys_by_sense = {s: keys for s, keys in keys_by_sense.items() if len(keys) >= 3}
+        all_senses = list(keys_by_sense.keys())
+        total_keys = sum(len(keys) for keys in keys_by_sense.values())
+        log(f"RECIPE: Using {total_keys} entries for {len(all_senses)} senses")
+        while True:
+            current_keys = copy.deepcopy(keys_by_sense)
+            while any(len(values) >= 3 for values in current_keys.values()):
+                sense = random.choice(all_senses)
+                key_a, key_b, key_c = random.sample(current_keys[sense], 3)
+                if len(set([key_a.lower(), key_b.lower(), key_c.lower()])) != 3:
+                    continue
+                sim_ab = s2v.similarity(key_a, key_b)
+                sim_ac = s2v.similarity(key_a, key_c)
+                if sim_ab < threshold or sim_ac < threshold:
+                    continue
+                current_keys[sense].remove(key_a)
+                current_keys[sense].remove(key_b)
+                current_keys[sense].remove(key_c)
+                confidence = 1.0 - (min(sim_ab, sim_ac) / max(sim_ab, sim_ac))
+                task = {
+                    "label": "Which one is more similar?",
+                    "html": html.format(*s2v.split_key(key_a)),
+                    "key": key_a,
+                    "options": [
+                        {
+                            "id": key_b,
+                            "html": html.format(*s2v.split_key(key_b)),
+                            "score": sim_ab,
+                        },
+                        {
+                            "id": key_c,
+                            "html": html.format(*s2v.split_key(key_c)),
+                            "score": sim_ac,
+                        },
+                    ],
+                    "confidence": confidence,
+                }
+                yield task
+
+    def on_exit(ctrl):
+        """Output summary about user agreement with the model."""
+        msg = Printer()
+        set_id = dataset if eval_whole else ctrl.session_id
+        data = ctrl.db.get_dataset(set_id)
+        data = [eg for eg in data if eg["answer"] == "accept" and eg.get("accept")]
+        if not data:
+            msg.warn("No annotations collected", exits=1)
+        agree_count = 0
+        for eg in data:
+            choice = eg["accept"][0]
+            score_choice = [o["score"] for o in eg["options"] if o["id"] == choice][0]
+            score_other = [o["score"] for o in eg["options"] if o["id"] != choice][0]
+            if score_choice > score_other:
+                agree_count += 1
+        pc = agree_count / len(data)
+        text = f"You agreed {agree_count} / {len(data)} times ({pc:.0%})"
+        msg.info(f"Evaluating data from '{set_id}'")
+        if pc > 0.5:
+            msg.good(text)
+        else:
+            msg.fail(text)
+
+    return {
+        "view_id": "choice",
+        "dataset": dataset,
+        "stream": get_stream(),
+        "on_exit": on_exit,
+        "config": {"choice_style": "single", "choice_auto_accept": True},
+    }
