995820c2f911f4aebaf197d1122fe1f16c5784c5
shademe <shadeMe@users.noreply.github.com>
2023-04-17 13:42:59 +0200

    Use `use_ops` context manager in test

--- sense2vec/tests/test_issue155.py
+++ sense2vec/tests/test_issue155.py
@@ -1,17 +1,13 @@
 from pathlib import Path
 import pytest
-import spacy
 from sense2vec.sense2vec import Sense2Vec
+from thinc.api import use_ops
 from thinc.util import has_cupy_gpu
 
 
 @pytest.mark.skipif(not has_cupy_gpu, reason="requires Cupy/GPU")
 def test_issue155():
     data_path = Path(__file__).parent / "data"
-    spacy.require_gpu()
-
-    s2v = Sense2Vec().from_disk(data_path)
-    s2v.most_similar("beekeepers|NOUN")
-
-    # Restore CPU ops for the rest of the session
-    spacy.require_cpu()
+    with use_ops("cupy"):
+        s2v = Sense2Vec().from_disk(data_path)
+        s2v.most_similar("beekeepers|NOUN")
